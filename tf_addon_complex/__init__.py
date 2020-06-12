d3beabc5c6c4a42611236c233c1c9c25f0864ada
mullerrwd <robert.muller@tno.nl>
2020-06-12 19:55:37 +0200

    Added basic versioning. Added the submodules as dirs.

--- tf_addon_complex/__init__.py
+++ tf_addon_complex/__init__.py
@@ -3,3 +3,11 @@
 __version__ = '0.0.1'
 __author__ = 'Robert Muller <mullerrwd@gmail.com>'
 __all__ = []
+
+# Import locals
+from tf_addon_complex import activations
+from tf_addon_complex import layers
+from tf_addon_complex import losses
+from tf_addon_complex import optimizers
+
+from tf_addon_complex.version import __version__
---
+++ tf_addon_complex/version.py
@@ -0,0 +1,12 @@
+__MAJOR__ = '0'
+__MINOR__ = '0'
+__PATCH__ = '0'
+
+__SUFFIX__ = 'dev'
+
+
+__version__ = '{}.{}.{}'.format(__MAJOR__, __MINOR__, __PATCH__)
+
+
+if __SUFFIX__:
+    __version__ = '{}_{}'.format(__version__, __SUFFIX__)
